201a5f0a9555339588b2ae49fe66761ef5ceeaea
v.chykharivskyi <v.chykharivskyi@gmail.com>
2020-07-06 17:13:41 +0300

    Updates

---
+++ precache-manifest.ae31577d715d4fd8599f38413fe7fa62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684a039d4d75597a4194305a64b38e4a",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "38939ed967a256bb7345",
+    "url": "/Quiz-App/static/css/main.cc63c310.chunk.css"
+  },
+  {
+    "revision": "b0a4079feaaf27388e97",
+    "url": "/Quiz-App/static/js/2.9a8c1cdc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Quiz-App/static/js/2.9a8c1cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38939ed967a256bb7345",
+    "url": "/Quiz-App/static/js/main.c60b0785.chunk.js"
+  },
+  {
+    "revision": "4845dedd9da576adbbe3",
+    "url": "/Quiz-App/static/js/runtime-main.5027db05.js"
+  }
+]);
